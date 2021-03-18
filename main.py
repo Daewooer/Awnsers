5e234b5a389c083e27bfe11cc8e5912b999f18c3
Kostas Zafolias <76820150+Daewooer@users.noreply.github.com>
2021-03-18 23:42:30 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+print("How is it going?")
+user_input = input()
+
+if user_input == "brm":
+    while 3 > 2:
+        print("XESTIKA")
+        break
+
+elif user_input == "exw katathlipsi":
+    while 3 > 2:
+        print("ok? Ti na kanw🤦‍")
+
+elif user_input == "den xerw":
+    while 3 > 1:
+        print("TI DEN XEREIS!")
+
+elif user_input == "ti na pw?":
+    while 3 > 1:
+        print("PES KATI MHN XESW!!!!!!!!!!!!")
+
+elif user_input == "thelw na anoixoun ta sxoleia":
+    while 3 > 1:
+        print("EGW DEN THELW")
+
+elif user_input == "xerw thn Sinti apo 3 xronwn":
+    while 5 > 1:
+        print("D-E-N M-E N-O-I-A-Z-E-I!")
+
+elif user_input == "ti na kanw?":
+    while 420 > 69:
+        print("fae")
+else:
+    print("I'm happy to hear that!")
+    print(":D")
